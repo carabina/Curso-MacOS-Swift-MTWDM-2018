c71df25f04e4735969598f8d080518c3c1716bbb
Felipe Hernandez Palafox <felipe.sistemas.fhp@gmail.com>
2018-07-06 18:04:12 -0500

    Aplicaciones MacOS del curso
    Desarrollo de aplicaciones MacOS con swift  MTWDM2018

---
+++ App1mtwdm/App1mtwdm/SegundaViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SegundaViewController.swift
+//  App1mtwdm
+//
+//  Created by Felipe Hernandez on 01/06/18.
+//  Copyright © 2018 Felipe Hernandez. All rights reserved.
+//
+
+import Cocoa
+
+class SegundaViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
